d4bf0130887af0e60916d3c91c316b05f4d6d44b
brunopradoprado <bruno.2006p@gmail.com>
2026-04-14 10:28:29 -0300

    alter posicao CampoTexto

--- src/componentes/CampoTexto/index.jsx
+++ src/componentes/CampoTexto/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import IconeLupa from "../componentes/IconeLupa"
+import IconeLupa from "../IconeLupa"
 
 const CampoTextoEstilizado = styled.div`
   display: flex;
